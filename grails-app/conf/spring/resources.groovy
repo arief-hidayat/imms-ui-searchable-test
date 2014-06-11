3d65d59f8d75e02ec599ea8fc3b5670c578d5e27
arief-hidayat <mr.arief.hidayat@gmail.com>
2014-06-11 14:23:29 +0800

    remove joda date converter. use config instead

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,11 +1,5 @@
-import com.hida.imms.JodaDateConverter
 
 // Place your Spring DSL code here
 beans = {
 
-//    for(Class jodaType : JodaDateConverter.SUPPORTED_TYPES) {
-//        "joda${jodaType.simpleName}Converter"(JodaDateConverter) {
-//            type = jodaType
-//        }
-//    }
 }
